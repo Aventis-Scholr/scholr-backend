637312aa18176261cddf450ce66d85d14413bab6
John <u202117377@upc.edu.pe>
2025-05-08 10:01:57 -0500

    chore: initial commit

---
+++ src/main/java/com/scholr/scholr_paltform/ScholrPaltformApplication.java
@@ -0,0 +1,13 @@
+package com.scholr.scholr_paltform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholrPaltformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholrPaltformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scholr/scholr_paltform/ScholrPaltformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholr.scholr_paltform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholrPaltformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
